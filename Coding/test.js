735af3a7e7b949a680f064da7c3d4b6a59c16d57
TanujTyagi <83901675+TanujTyagi@users.noreply.github.com>
2025-04-12 18:45:05 +0530

    Update script.js

--- Coding/test.js
+++ Coding/test.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (e.target.id == e.target.id) {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
